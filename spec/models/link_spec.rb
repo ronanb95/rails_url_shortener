58d5c5886828736e2665cd66610124a03e3739ab
Ronan Byrne <ronan.byrne1@ucdconnect.ie>
2021-02-08 13:14:11 +0000

    Created validation for URL input

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,12 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Link, type: :model do
     
-    it "it is valid if it always have an original URL and a short_code" do 
+    it "it is valid if it always have an URL and a short_code" do 
         link = Link.new(
             original_url: "https://gapfish.com/welcome/",
             lookup_code: "1234567"
         )
         expect(link.valid?).to be(true)
+    end
+
+    it "it is invalid if the original URL is not formatted correctly" do 
+        link = Link.new(
+            original_url: "testtest",
+            lookup_code: "1234567"
+        )
+        expect(link.valid?).to be(false)
     end
 
     it "it is invalid if it does not have a short_code" do 
@@ -34,20 +42,6 @@ RSpec.describe Link, type: :model do
         link2 = Link.new(
             original_url: "https://gapfish.com/about/",
             lookup_code: "1234567"
-        )
-        expect(link2.valid?).to be(false)
-    end
-
-    it "it is valid if the original_url provided is unique" do 
-        link = Link.new(
-            original_url: "https://gapfish.com/welcome/",
-            lookup_code: "1234567"
-        )
-        link.save
-
-        link2 = Link.new(
-            original_url: "https://gapfish.com/welcome/",
-            lookup_code: "7654321"
         )
         expect(link2.valid?).to be(false)
     end
